fce3aac275484c26a36ec71b9edc4952a7633bed
Nutapol Thungpao <57092203+nutapol97@users.noreply.github.com>
2022-05-06 14:15:15 +0700

    Update car_racing.py

--- gym/envs/box2d/car_racing.py
+++ gym/envs/box2d/car_racing.py
@@ -193,8 +193,8 @@ def default_reward_callback(env):
 
     re_p,sum_obc_touch = env.check_obstacles_touched()
     reward += re_p
-    print("tile_visited_count : {}".foramat(env.tile_visited_count))
-    print("env.track : {}".foramat(env.track))
+    print("tile_visited_count : {}".format(env.tile_visited_count))
+    print("env.track : {}".format(env.track))
     if env.tile_visited_count == len(env.track):
         lap_count +=1
         reward += 100
